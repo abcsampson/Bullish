a839be5cf5525c9e19faaa32678f45ee5816f44a
abcsampson <abcsampson@gmail.com>
2024-01-28 23:27:33 +0800

    Initial commit

---
+++ src/test/java/com/bullish/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bullish.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
